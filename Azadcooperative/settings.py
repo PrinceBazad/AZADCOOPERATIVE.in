69dbf6d83a1de91e0f9d833ce398998a11ccf49a
PrinceBazad <princebazad3@gmail.com>
2025-05-23 17:22:41 +0530

    Update static files configuration

--- Azadcooperative/settings.py
+++ Azadcooperative/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1ox)be-^=^)h=3o_=by42aofwaxz+#d0$)6ksemk%wog9&5(if
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
