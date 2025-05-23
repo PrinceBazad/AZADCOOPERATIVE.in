0f8247a726b4d0ae53f20530baaf9617f0c59d53
PrinceBazad <princebazad3@gmail.com>
2025-05-23 16:22:47 +0530

    Update project files

--- Azadcooperative/urls.py
+++ Azadcooperative/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .import views
+from django.shortcuts import redirect
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.index,name='index'),
-    path('website/',include('website.urls'))
+    path('', RedirectView.as_view(url='website/')),
+    path('website/', include('website.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Azadcooperative/views.py
+++ Azadcooperative/views.py
@@ -1,9 +1,3 @@
 # I have created this -prince
 from django.http import HttpResponse
 from django.shortcuts import render
-
-def index(request):
-    return HttpResponse("Welcome to the main project page")
-
-def about(request):
-    return HttpResponse("About")
